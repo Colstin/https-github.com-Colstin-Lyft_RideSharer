a7ecebdfda9978df04b2fa58cbe23ea8e09672aa
Colstin Donaldson <colstin50@gmail.com>
2023-11-15 11:56:44 -0800

    Initial Commit

---
+++ RideShare/RideShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  RideShareApp.swift
+//  RideShare
+//
+//  Created by Colstin Donaldson on 11/15/23.
+//
+
+import SwiftUI
+
+@main
+struct RideShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
